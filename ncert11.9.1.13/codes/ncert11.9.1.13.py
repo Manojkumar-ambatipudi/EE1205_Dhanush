8aaa11f993e7a136e1cb3994b9f4b35f341c9899
Dhanush V Nayak <dhanushnayakh03@gmail.com>
2024-02-01 20:18:32 +0530

    update

--- ncert11.9.1.13/codes/ncert11.9.1.13.py
+++ ncert11.9.1.13/codes/ncert11.9.1.13.py
@@ -1,21 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Read data from the file using numpy
-data_file = "output_data.txt"
-data = np.loadtxt(data_file, skiprows=1)  # Skip the header
+# Load data from file
+data = np.loadtxt("output_data.txt", skiprows=1)
 
 # Extract columns
-n_values = data[:, 0]
-x_values = data[:, 1]
+n = data[:, 0]
+x_n = data[:, 1]
+y_n = data[:, 2]
 
-#close all the previous figs
-plt.close("all")
-
-# Plot the data using matplotlib
-plt.stem(n_values, x_values, basefmt=" ", linefmt="-", markerfmt="o")
+# Plotting with adjusted marker size and different colors for better visibility
+plt.stem(n, y_n, linefmt='r-', markerfmt='ro', basefmt='r-', label=r'Simulation')
+plt.scatter(n, x_n, color='green',marker='x',s=100, label=r'Theory')
 plt.xlabel('n')
-plt.ylabel('x(n)')
-plt.title('Plot of x(n)')
-plt.grid(True)
-plt.savefig("fig_x(n).png")
+plt.ylabel('Function Values')
+plt.title('Stem Plot of Theory and Simulation')
+plt.legend()
+plt.savefig("Theory_vs_Simulation")
