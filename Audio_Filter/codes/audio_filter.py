1bf6bc21c5a5e52f468669584b3e2a63c1145d98
Dhanush V Nayak <dhanushnayakh03@gmail.com>
2024-03-25 14:55:02 +0530

    update

--- Audio_Filter/codes/audio_filter.py
+++ Audio_Filter/codes/audio_filter.py
@@ -2,7 +2,7 @@ import soundfile as sf
 import numpy as np
 from scipy import signal
 #read .wav file 
-input_signal,fs = sf.read('Dhanush-Singing.wav') 
+input_signal,fs = sf.read(r'D:\2and sem IITH\Signals and system\Audio Filter\codes\Sound_Noise.wav') 
 
 #sampling frequency of Input signal
 sampl_freq=fs
@@ -12,17 +12,18 @@ sampl_freq=fs
 order=4
 
 #cutoff frquency 
-cutoff_freq=1000.0  
+cutoff_freq=900.0  
 
 #digital frequency
 Wn=2*cutoff_freq/sampl_freq  
 
 # b and a are numerator and denominator polynomials respectively
 b, a = signal.butter(order, Wn, 'low') 
-
+print(b)
+print(a)
 #filter the input signal with butterworth filter
-output_signal = signal.filtfilt(b, a, input_signal)
-
+output_signal = signal.filtfilt(b, a, input_signal,padlen=1)
+#output_signal = signal.lfilter(b, a, input_signal)
 
 #write the output signal into .wav file
 sf.write('Sound_With_ReducedNoise.wav', output_signal, fs) 
